fb546305aa7ecdfc42977457cd1c443f9c438a93
camalzade_elvin <camalzade_elvin@outlook.com>
2019-05-09 03:11:19 +0500

    Entities are ready . . .

--- Library/Entities/Book.cs
+++ Library/Entities/Book.cs
@@ -10,6 +10,12 @@ namespace Library.Entities
     {
         public int Id { get; set; }
         public int No { get; set; }
-        public int MyProperty { get; set; }
+        public string Name { get; set; }
+        public Author Author { get; set; }
+        public Filial Filial { get; set; }
+        public Genre Genre { get; set; }
+        public int PageCount { get; set; }
+        public double PurchasePrice { get; set; }
+        public double SalePrice { get; set; }
     }
 }
