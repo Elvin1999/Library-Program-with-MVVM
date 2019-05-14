17b48405c4439f83a937cd5a760cf8d258b46261
camalzade_elvin <camalzade_elvin@outlook.com>
2019-05-14 20:58:01 +0400

    Sale and Rent Classes

---
+++ Library/Entities/Rent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Entities
+{
+   public class Rent
+    {
+        public int Id { get; set; }
+        public int No { get; set; }
+        public Client Client { get; set; }
+        public double Price_Per_Day { get; set; }
+        public string Note { get; set; }
+       
+    }
+}
--- Library/ViewModels/SaleViewModel.cs
+++ Library/ViewModels/SaleViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Library.ViewModels
 {
-    public class SellViewModel : BaseViewModel
+    public class SaleViewModel : BaseViewModel
     {
 
     }
